2d03ac1c21543f75ba001d1f6be83e0c834050fd
thomas.meslin <thomas.meslin@developpement-durable.gouv.fr>
2026-05-13 18:03:32 +0200

    hydrometry/logie

--- get-all-station-hydrometry.py
+++ get-all-station-hydrometry.py
@@ -3,9 +3,11 @@ from pathlib import Path
 
 # dossier vers lequel mettre les résultats
 dest_folder = Path("output")
-
-df = hydrometry.get_all_stations(fields=["resultat_obs_elab", "code_site", "date_obs_elab"])
-dg = hydrometry.get_observations()
+#fields=["resultat_obs_elab", "code_site", "date_obs_elab"]
+#fields=[]
+df = hydrometry.get_all_stations()
 df.to_csv(dest_folder / 'stations.csv')
-dg.to_csv(dest_folder / 'observations.csv')
+
+#dg = hydrometry.get_observations()
+#dg.to_csv(dest_folder / 'observations.csv')
 
