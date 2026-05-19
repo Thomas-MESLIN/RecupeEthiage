3fb515a02550331860457a69d517edd69876c80d
thomas.meslin <thomas.meslin@developpement-durable.gouv.fr>
2026-05-19 18:35:19 +0200

    Test gather auto hydroportail. Prêt a automatiser l'importation de hydroportail et de scrapper tout le site !!

---
+++ test-gather-auto-hydroportail.py
@@ -0,0 +1,120 @@
+import requests
+import pandas as pd
+from pathlib import Path
+
+# ==========================================
+# PARAMÈTRES
+# ==========================================
+
+def download_hydro(sandre_code:str,start_date:str):
+    rdds = sandre_code
+    start_at = start_date
+    output_folder = Path("output/exports_hydro")
+
+    # ==========================================
+    # URL API
+    # ==========================================
+
+    url = "https://hydro.eaufrance.fr/api/carte/donnees"
+
+    params = {
+        "data": "qmm",
+        "startAt": start_at,
+        "status": "validated",
+        "rdds[]": rdds,
+        "includeClosedEntities": "false",
+    }
+
+    # ==========================================
+    # HEADERS NAVIGATEUR
+    # ==========================================
+
+    headers = {
+        "User-Agent": (
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/136.0.0.0 Safari/537.36"
+        ),
+        "Accept": "application/json, text/plain, */*",
+        "Referer": (
+            "https://hydro.eaufrance.fr/carte-donnees/carte/toutes-eaux"
+        ),
+        "Origin": "https://hydro.eaufrance.fr",
+    }
+
+    # ==========================================
+    # SESSION
+    # ==========================================
+
+    session = requests.Session()
+
+    # IMPORTANT :
+    # ouvrir d'abord la page web pour obtenir les cookies
+    session.get(
+        "https://hydro.eaufrance.fr/carte-donnees/carte/toutes-eaux",
+        headers=headers,
+    )
+
+    # ensuite appeler l'API
+    response = session.get(
+        url,
+        params=params,
+        headers=headers,
+    )
+
+    print(response.status_code)
+    print(response.url)
+
+    response.raise_for_status()
+
+    # ==========================================
+    # JSON
+    # ==========================================
+
+    data = response.json()
+
+    features = data["features"]
+
+    rows = []
+
+    for feature in features:
+
+        properties = feature.get("properties", {})
+        geometry = feature.get("geometry", {})
+
+        coordinates = geometry.get("coordinates", [None, None])
+
+        row = {
+            **properties,
+            "longitude": coordinates[0],
+            "latitude": coordinates[1],
+            "geometry_type": geometry.get("type"),
+        }
+
+        rows.append(row)
+
+    df = pd.DataFrame(rows)
+    #df = df[df["entityType"] == "site"]
+
+    # ==========================================
+    # EXPORT
+    # ==========================================
+
+    output_file = output_folder / f"{start_at[:7]}-{rdds}-only-validated-qmm.csv"
+
+    df.to_csv(
+        output_file,
+        index=False,
+        encoding="utf-8"
+    )
+
+    print(df.head())
+    print(f"\nCSV sauvegardé : {output_file}")
+
+
+rdds = "BSH101"
+start_at = "2001-01-01T00:00:00Z"
+download_hydro(rdds,start_at)
+rdds = "BSH001"
+start_at = "2001-01-01T00:00:00Z"
+download_hydro(rdds,start_at)
